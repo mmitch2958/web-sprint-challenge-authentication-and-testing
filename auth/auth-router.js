90b5fd5d088793f9cf60e1bc8b0a6c7276b80c57
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-16 11:26:03 -0400

    installed dep, updated json and knexfile

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,43 @@
 const router = require('express').Router();
+const bcryptjs = require('bcryptjs');
+const db = require('../database/dbConfig.js');
+const generateToken = require('./generateToken.js');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  if(!username || !password) return res.status(400).json({ error: 'Provide both a username and password' });
+
+  const hash = bcryptjs.hashSync(password, process.env.HASH_ROUNDS || 8);
+
+  try {
+    await db('users').insert({ username, password: hash })
+    const user = await db('users').where({ username }).first();
+    res.status(201).json(user);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Could not add user to database' });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) return res.status(400).json({ error: 'Provide both a username and password' });
+
+  try {
+    const user = await db('users').where({ username }).first();
+    if(user && bcryptjs.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: `Welcome ${user.username}`, token });
+    } else {
+      res.status(401).json({ error: 'Password is incorrect' });
+    }
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ message: "Could not find user in database" });
+  }
 });
+
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,15 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if(token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decoded) => {
+      if (err) return res.status(401).json({ error: 'incorrect token provided'});
+      next();
+    })
+  } else {
+    return res.status(400).json({ message: 'please provide a token'});
+  }  
 };
