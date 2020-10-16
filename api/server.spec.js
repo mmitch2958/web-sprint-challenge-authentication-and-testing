d13ddb76d97473f8dc22f28dce918df51a035ec3
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-16 12:30:45 -0400

    MVP + 3of4Stretch

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const server = require('./server.js');
 const db = require('../database/dbConfig.js');
 
+//API Auth 
+
 describe('auth-router.js', () => {
     beforeEach(async () => {
         await db('users').truncate();
@@ -11,30 +13,51 @@ describe('auth-router.js', () => {
     it('Test will use testing environment', () => {
         expect(process.env.DB_ENV).toBe('testing');
     });
-  
-    describe('Register and Return new user', () => {
-        it('will recieve 201 status on success', async () => {
-            const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
-  
-            expect(response.status).toEqual(201);
-        });
-  
-        it('Test will return object with username on success', async () => {
-            const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
-  
-            expect(response.body).toHaveProperty('username', "test");
-        });
-  
-        it('Test will return a 400 error if missing username', async () => {
-            const response = await request(server).post('/api/auth/register').send({ password: 'pass' });
-  
-            expect(response.status).toEqual(400);
-        });
-  
-        it('Test will return a 500 error if username already taken', async () => {
-            await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
-            const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass2' });
-  
-            expect(response.status).toEqual(500);
-        });
+
+//Register a User 
+   
+describe('Register and Return new user', () => {
+    it('will recieve 201 status on success', async () => {
+        const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+
+        expect(response.status).toEqual(201);
     });
+
+    it('Test will return object with username on success', async () => {
+        const response = await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+
+        expect(response.body).toHaveProperty('username', "test");
+    });
+
+    it('Test will return a 400 error if missing username', async () => {
+        const response = await request(server).post('/api/auth/register').send({ password: 'pass' });
+
+        expect(response.status).toEqual(400);
+    });
+
+    //API JOKES 
+    describe('Return jokes on /api/jokes get request', () => {
+        it('Recieve 200 status on success', async () => {
+            await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+            const userResponse = await request(server).post('/api/auth/login').send({ username: 'test', password: 'pass' });
+            const response = await request(server).get('/api/jokes').set({ Authorization: userResponse.body.token });
+    
+            expect(response.status).toEqual(200);
+        })
+    
+        it('Return a List of 20 jokes', async () => {
+            await request(server).post('/api/auth/register').send({ username: 'test', password: 'pass' });
+            const userResponse = await request(server).post('/api/auth/login').send({ username: 'test', password: 'pass' });
+            const response = await request(server).get('/api/jokes').set({ Authorization: userResponse.body.token });
+    
+            expect(response.body).toHaveLength(20);
+        })
+    
+        it('Return 400 when there is not a token provided', async () => {
+            const response = await request(server).get('/api/jokes');
+    
+            expect(response.status).toEqual(400);
+        })
+    
+    })
+});
